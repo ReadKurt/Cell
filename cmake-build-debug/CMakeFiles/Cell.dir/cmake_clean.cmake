01f09b429c83b0832f877c0f62066fff53149824
WJ <hustwj1996@gmail.com>
2017-05-26 23:24:04 +0800

    create Cell

---
+++ cmake-build-debug/CMakeFiles/Cell.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cell.dir/main.cpp.obj"
+  "Cell.pdb"
+  "Cell.exe"
+  "Cell.exe.manifest"
+  "libCell.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
