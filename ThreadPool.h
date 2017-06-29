7e08d76bda100ab6752c5b19cd7127e9bbc31d6d
WJ <hustwj1996@gmail.com>
2017-06-29 23:19:53 +0800

    ThreadPool

---
+++ ThreadPool.h
@@ -0,0 +1,100 @@
+//
+// Created by WJ on 2017/6/29.
+//
+
+#ifndef CELL_THREADPOOL_H
+#define CELL_THREADPOOL_H
+
+
+#include <vector>
+#include <queue>
+#include <memory>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <future>
+#include <functional>
+#include <stdexcept>
+
+using namespace std;
+
+class ThreadPool {
+public:
+    ThreadPool(size_t);
+
+    template<class F, class ...Args>
+    auto enqueue(F &&f, Args &&... args) -> std::future<typename std::result_of<F(Args...)>::type>;
+
+    ~ThreadPool();
+
+private:
+    vector<thread> workers;
+
+    queue<function<void()> > tasks;
+    mutex queue_mutex;
+    condition_variable condition;
+    bool stop;
+
+};
+
+inline ThreadPool::ThreadPool(size_t threads) : stop(false) {
+    for (size_t i = 0; i < threads; i++) {
+        workers.emplace_back(
+                [this] {
+                    for (;;) {
+                        function<void()> task;
+                        {
+                            unique_lock<mutex> lock(this->queue_mutex);
+                            this->condition.wait(lock, [this] { return this->stop || !this->tasks.empty(); });
+                            if (this->stop && this->tasks.empty())
+                                return;
+                            task = std::move(this->tasks.front());
+                            this->tasks.pop();
+                        }
+                        task();
+                    }
+                }
+        );
+    }
+}
+
+
+template<class F, class... Args>
+auto ThreadPool::enqueue(F&& f, Args&&... args)
+-> std::future<typename std::result_of<F(Args...)>::type>
+{
+    using return_type = typename result_of<F(Args...)>::type;
+    auto task = make_shared<packaged_task<return_type ()>>(bind(forward<F>(f),forward<Args>(args)...));
+
+    future<return_type > res = task->get_future();
+
+    {
+        unique_lock<mutex> lock(queue_mutex);
+
+        if(stop)
+            throw runtime_error("enqueue on stopped ThreadPool");
+
+        tasks.emplace([task](){ (*task)(); });
+    }
+
+    condition.notify_all();
+    return res;
+};
+
+
+inline ThreadPool::~ThreadPool() {
+    {
+        unique_lock<mutex> lock(queue_mutex);
+
+        stop = true;
+
+    }
+
+    condition.notify_all();
+
+    for(auto &worker: workers)
+        worker.join();
+}
+
+
+#endif //CELL_THREADPOOL_H
